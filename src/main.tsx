e243cd5b2bfb1ba25e5d747ca566c8bb42d7d7ca
Mohammad-Nafis-Abdullah <nafisabdullah2@gmail.com>
2023-07-02 16:18:10 +0600

    connect with new firebase project

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import { GetRoutes } from './routes/routes'
-import { RouterProvider } from 'react-router-dom'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { GetRoutes } from './routes/routes';
+import { RouterProvider } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
